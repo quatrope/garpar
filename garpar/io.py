0dacebab212af641b353a9c81f862033cfbe61cb
JuanBC <jbc.develop@gmail.com>
2021-12-17 01:22:17 -0300

    hdf5 works

--- garpar/io.py
+++ garpar/io.py
@@ -78,7 +78,7 @@ def _df_to_sarray(df):
 # =============================================================================
 
 
-def to_hdf5(path_or_stream, portfolio, dataset="portfolio", **kwargs):
+def to_hdf5(path_or_stream, portfolio, group="portfolio", **kwargs):
     """HDF5 file writer.
 
     It is responsible for storing a portfolio in HDF5 format.
@@ -89,13 +89,12 @@ def to_hdf5(path_or_stream, portfolio, dataset="portfolio", **kwargs):
         Path or file like objet to the h5 to store the portfolio.
     portfolio : garpar.portfolio.PortFolio
         The portfolio to store.
-    dataset : str (default="portfolio")
-        The name of the dataset where the portfolio will be stored.
+    group : str (default="portfolio")
+        The name of the group where the portfolio will be stored.
     kwargs :
         Extra arguments to the function ``h5py.File.create_dataset``.
 
     """
-    sarray = _df_to_sarray(portfolio._df)
 
     # # prepare metadata
     h5_metadata = _DEFAULT_METADATA.copy()
@@ -106,14 +105,28 @@ def to_hdf5(path_or_stream, portfolio, dataset="portfolio", **kwargs):
     kwargs.setdefault("compression_opts", 9)
 
     with h5py.File(path_or_stream, "a") as h5:
-        ds = h5.create_dataset(dataset, data=sarray, **kwargs)
-        ds.attrs.update(portfolio.metadata)
+        grp = h5.create_group(group)
+        grp.create_dataset(
+            f"{group}_df", data=_df_to_sarray(portfolio._df), **kwargs
+        )
+        grp.create_dataset(
+            f"{group}_weights", data=portfolio._weights, **kwargs
+        )
+
+        grp.attrs.update(portfolio.metadata)
 
         h5.attrs.update(h5_metadata)
 
 
-def read_hdf5(path_or_stream, dataset="portfolio"):
+def read_hdf5(path_or_stream, group="portfolio"):
     with h5py.File(path_or_stream, "r") as h5:
-        ds = h5[dataset]
-        df = pd.DataFrame(ds[:])
-        return portfolio.Portfolio.from_dfkws(df, **ds.attrs)
+
+        grp = h5[group]
+
+        df_ds = grp[f"{group}_df"]
+        df = pd.DataFrame(df_ds[:])
+
+        weights_ds = grp[f"{group}_weights"]
+        weights = weights_ds[:]
+
+        return portfolio.Portfolio.from_dfkws(df, weights, **grp.attrs)
