f857425dd13c16a6c094d6ceb4b7a6b3585e203d
juanbc <jbcabral@unc.edu.ar>
2023-04-24 23:51:36 -0300

    optimizers

--- garpar/optimize.py
+++ garpar/optimize.py
@@ -9,7 +9,7 @@ import pypfopt
 import numpy as np
 
 from .core import Portfolio
-from .utils.mabc import ModelABC, abstractmethod, hparam
+from .utils import Bunch, mabc
 
 
 # =============================================================================
@@ -17,10 +17,14 @@ from .utils.mabc import ModelABC, abstractmethod, hparam
 # =============================================================================
 
 
-class OptimizerABC(ModelABC):
-    @abstractmethod
-    def optimize(self, pf):
+class OptimizerABC(mabc.ModelABC):
+    @mabc.abstractmethod
+    def _calculate_weights(self, pf):
         raise NotImplementedError()
+
+    def optimize(self, pf):
+        weights, metadata = self._calculate_weights(pf)
+        return pf.copy(weights=weights, optimizer=metadata)
 
 
 # =============================================================================
@@ -38,88 +42,82 @@ class Markowitz(OptimizerABC):
 
     """
 
-    target_return = hparam(default=None)
+    target_return = mabc.hparam(default=None)
 
-    weight_bounds = hparam(default=(0, 1))
-    market_neutral = hparam(default=False)
+    weight_bounds = mabc.hparam(default=(0, 1))
+    market_neutral = mabc.hparam(default=False)
 
-    returns = hparam(default="mah")
-    returns_kw = hparam(factory=dict)
+    returns = mabc.hparam(default="mah")
+    returns_kw = mabc.hparam(factory=dict)
 
-    covariance = hparam(default="sample_cov")
-    covariance_kw = hparam(factory=dict)
+    covariance = mabc.hparam(default="sample_cov")
+    covariance_kw = mabc.hparam(factory=dict)
 
-    def _to_kwargs(self, pf):
-        mu = pf.ereturns(self.returns, **self.returns_kw)
-        cov = pf.covariance(self.covariance, **self.covariance_kw)
-        return {
-            "expected_returns": mu,
-            "cov_matrix": cov,
-            "weight_bounds": self.weight_bounds,
-        }
+    def _get_optimizer(self, pf):
+        expected_returns = pf.ereturns(self.returns, **self.returns_kw)
+        cov_matrix = pf.covariance(self.covariance, **self.covariance_kw)
+        weight_bounds = self.weight_bounds
+        optimizer = pypfopt.EfficientFrontier(
+            expected_returns=expected_returns,
+            cov_matrix=cov_matrix,
+            weight_bounds=weight_bounds,
+        )
+        return optimizer
 
     def _coerce_target_return(self, port):
         if self.target_return is None:
-            returns = port.as_prices().to_numpy()
-            return np.min(returns)
+            returns = port.as_returns().to_numpy()
+            return np.min(np.abs(returns))
         return self.target_return
 
-    def optimize(self, pf):
-        kwargs = self._to_kwargs(pf)
+    def _calculate_weights(self, pf):
+        optimizer = self._get_optimizer(pf)
         target_return = self._coerce_target_return(pf)
+        market_neutral = self.market_neutral
 
-        ef = pypfopt.EfficientFrontier(**kwargs)
-
-        weights = ef.efficient_return(target_return, self.market_neutral)
-
-        weights_list = [weights[stock] for stock in self._prices_df.columns]
-
-        metadata = dict(pf.metadata)
-        metadata.update(
-            optimizer=type(self).__name__,
-            optimizer_kwargs={"target_return": target_return},
+        weights_dict = optimizer.efficient_return(
+            target_return, market_neutral=market_neutral
         )
+        weights = [weights_dict[stock] for stock in pf.stocks]
 
-        return Portfolio.from_dfkws(
-            self._prices_df.copy(), weights_list, **metadata
-        )
+        optimizer_metadata = {
+            "name": type(self).__name__,
+            "target_return": target_return,
+        }
+
+        return weights, optimizer_metadata
 
 
 class BlackLitterman(OptimizerABC):
-    risk_aversion = hparam(default=None)
+    risk_aversion = mabc.hparam(default=None)
+    prior = mabc.hparam(default="equal")
+    absolute_views = mabc.hparam(default=None)
+    P = mabc.hparam(default=None)
+    Q = mabc.hparam(default=None)
+    covariance = mabc.hparam(default="sample_cov")
+    covariance_kw = mabc.hparam(factory=dict)
 
-    prior = hparam(default="equal")
-    absolute_views = hparam(default=None)
-    P = hparam(default=None)
-    Q = hparam(default=None)
-    covariance = hparam(default="sample_cov")
-    covariance_kw = hparam(factory=dict)
-
-    def _to_kwargs(self, pf):
+    def _get_optimizer(self, pf):
         cov = pf.covariance(self.covariance, **self.covariance_kw)
-        return {
-            "cov_matrix": cov,
-            "pi": self.prior,
-            "absolute_views": self.absolute_views,
-            "P": self.P,
-            "Q": self.Q,
-        }
+        prior = self.prior
+        absolute_views = self.absolute_views
+        P = self.P
+        Q = self.Q
 
-    def optimize(self, pf):
-        kwargs = self._to_kwargs(pf)
+        return pypfopt.BlackLittermanModel(
+            cov_matrix=cov, pi=prior, absolute_views=absolute_views, P=P, Q=Q
+        )
+
+    def _calculate_weights(self, pf):
+        blm = self._get_optimizer(pf)
         risk_aversion = self.risk_aversion
 
-        blm = pypfopt.BlackLittermanModel(**kwargs)
-        weights = blm.bl_weights(risk_aversion)
+        weights_dict = blm.bl_weights(risk_aversion)
+        weights = [weights_dict[stock] for stock in pf.stocks]
 
-        weights_list = [weights[stock] for stock in self._prices_df.columns]
+        optimizer_metadata = {
+            "name": type(self).__name__,
+            "risk_aversion": risk_aversion,
+        }
 
-        metadata = dict(pf.metadata)
-        metadata.update(
-            optimizer=type(self).__name__,
-            optimizer_kwargs={"risk_aversion": risk_aversion},
-        )
-
-        return Portfolio.from_dfkws(
-            self._prices_df.copy(), weights_list, **metadata
-        )
+        return weights, optimizer_metadata
