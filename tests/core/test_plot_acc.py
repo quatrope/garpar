7ea473a6eb02dfd836a6f00974268ac55403a45c
Divasco <diego.gimenez@unc.edu.ar>
2024-08-28 19:03:13 -0300

    Added test plot

--- tests/core/test_plot_acc.py
+++ tests/core/test_plot_acc.py
@@ -14,134 +14,158 @@ import pytest
 
 import seaborn as sns
 
-class TestPortfolioPlotter:
-    @pytest.fixture(autouse=True)
-    def setup_plotter(self, risso_portfolio, distribution):
-        self.pf = risso_portfolio(random_state=3, distribution=distribution)
-        self.plotter = PortfolioPlotter(self.pf)
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_line(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_line(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.line(returns=returns, ax=ax_test)
+    ax_test = fig_test.subplots()
+    plotter.line(returns=returns, ax=ax_test)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        sns.lineplot(data=data, ax=ax_ref)
-        ax_ref.set_title(title)
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    sns.lineplot(data=data, ax=ax_ref)
+    ax_ref.set_title(title)
 
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_heatmap(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.heatmap(returns=returns, ax=ax_test)
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_heatmap(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        ax_ref = sns.heatmap(data=data)
-        ax_ref.set_title(title)
+    ax_test = fig_test.subplots()
+    plotter.heatmap(returns=returns, ax=ax_test)
 
-
-    @check_figures_equal()
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_wheatmap(self, fig_test, fig_ref):
-        ax_test = fig_test.subplots()
-        self.plotter.wheatmap(ax=ax_test)
-
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._wdf()
-        ax_ref = sns.heatmap(data=data.T)
-        ax_ref.set_title(title)
-        ax_ref.set_xlabel("Stocks")
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    ax_ref = sns.heatmap(data=data)
+    ax_ref.set_title(title)
 
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_hist(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.hist(returns=returns, ax=ax_test)
+@check_figures_equal()
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_wheatmap(risso_portfolio, fig_test, fig_ref, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        ax_ref = sns.histplot(data=data)
-        ax_ref.set_title(title)
+    ax_test = fig_test.subplots()
+    plotter.wheatmap(ax=ax_test)
 
-
-    @check_figures_equal()
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_whist(self, fig_test, fig_ref):
-        ax_test = fig_test.subplots()
-        self.plotter.whist(ax=ax_test)
-
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._wdf()
-        ax_ref = sns.histplot(data=data.T)
-        ax_ref.set_title(title)
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._wdf()
+    ax_ref = sns.heatmap(data=data.T)
+    ax_ref.set_title(title)
+    ax_ref.set_xlabel("Stocks")
 
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_box(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.box(returns=returns, ax=ax_test)
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_hist(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        ax_ref = sns.boxplot(data=data)
-        ax_ref.set_title(title)
+    ax_test = fig_test.subplots()
+    plotter.hist(returns=returns, ax=ax_test)
 
-
-    @check_figures_equal()
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_wbox(self, fig_test, fig_ref):
-        ax_test = fig_test.subplots()
-        self.plotter.wbox(ax=ax_test)
-
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._wdf()
-        ax_ref = sns.boxplot(data=data)
-        ax_ref.set_title(title)
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    ax_ref = sns.histplot(data=data)
+    ax_ref.set_title(title)
 
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_kde(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.kde(returns=returns, ax=ax_test)
+@check_figures_equal()
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_whist(risso_portfolio, fig_test, fig_ref, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        ax_ref = sns.kdeplot(data=data)
-        ax_ref.set_title(title)
+    ax_test = fig_test.subplots()
+    plotter.whist(ax=ax_test)
 
-
-    @check_figures_equal()
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_wkde(self, fig_test, fig_ref):
-        ax_test = fig_test.subplots()
-        self.plotter.wkde(ax=ax_test)
-
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._wdf()
-        ax_ref = sns.kdeplot(data=data)
-        ax_ref.set_title(title)
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._wdf()
+    ax_ref = sns.histplot(data=data.T)
+    ax_ref.set_title(title)
 
 
-    @check_figures_equal()
-    @pytest.mark.parametrize("returns", [True, False])
-    @pytest.mark.parametrize("distribution", pytest.DISTRIBUTIONS)
-    def test_ogive(self, fig_test, fig_ref, returns):
-        ax_test = fig_test.subplots()
-        self.plotter.ogive(returns=returns, ax=ax_test)
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_box(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
 
-        ax_ref = fig_ref.subplots()
-        data, title = self.plotter._ddf(returns=returns)
-        ax_ref = sns.ecdfplot(data=data)
-        ax_ref.set_title(title)
+    ax_test = fig_test.subplots()
+    plotter.box(returns=returns, ax=ax_test)
+
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    ax_ref = sns.boxplot(data=data)
+    ax_ref.set_title(title)
+
+
+@check_figures_equal()
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_wbox(risso_portfolio, fig_test, fig_ref, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
+
+    ax_test = fig_test.subplots()
+    plotter.wbox(ax=ax_test)
+
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._wdf()
+    ax_ref = sns.boxplot(data=data)
+    ax_ref.set_title(title)
+
+
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_kde(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
+
+    ax_test = fig_test.subplots()
+    plotter.kde(returns=returns, ax=ax_test)
+
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    ax_ref = sns.kdeplot(data=data)
+    ax_ref.set_title(title)
+
+
+@check_figures_equal()
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_wkde(risso_portfolio, fig_test, fig_ref, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
+
+    ax_test = fig_test.subplots()
+    plotter.wkde(ax=ax_test)
+
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._wdf()
+    ax_ref = sns.kdeplot(data=data)
+    ax_ref.set_title(title)
+
+
+@check_figures_equal()
+@pytest.mark.parametrize("returns", [True, False])
+@pytest.mark.parametrize("price_distribution", pytest.DISTRIBUTIONS)
+def test_PortFolioPlotter_ogive(risso_portfolio, fig_test, fig_ref, returns, price_distribution):
+    pf = risso_portfolio(random_state=3, distribution=price_distribution)
+    plotter = PortfolioPlotter(pf)
+
+    ax_test = fig_test.subplots()
+    plotter.ogive(returns=returns, ax=ax_test)
+
+    ax_ref = fig_ref.subplots()
+    data, title = plotter._ddf(returns=returns)
+    ax_ref = sns.ecdfplot(data=data)
+    ax_ref.set_title(title)
