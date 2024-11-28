c8cc5523ddf2299690b9b67d356d255d3763289d
Divasco <diego.gimenez@unc.edu.ar>
2024-11-28 19:11:28 -0300

    Updating documentation

--- garpar/utils/scalers.py
+++ garpar/utils/scalers.py
@@ -13,16 +13,6 @@ import numpy as np
 def proportion_scaler(arr):
     """
     Scale the input array by dividing each element by the sum of the array.
-
-    Parameters
-    ----------
-    arr : array-like
-        Input array to be scaled.
-
-    Returns
-    -------
-    array-like
-        Scaled array where each element is divided by the sum of the parameter.
     """
     arr = np.asarray(arr)
     asum = np.sum(arr)
@@ -32,17 +22,6 @@ def proportion_scaler(arr):
 def minmax_scaler(arr):
     """
     Scales the input array using the min-max normalization technique.
-
-    Parameters
-    ----------
-    arr : array-like
-        The input array to be scaled.
-
-    Returns
-    -------
-    array-like
-        The scaled array where each element is normalized to the range [0, 1]
-        based on the minimum and maximum values of the input array.
     """
     arr = np.asarray(arr)
     amin, amax = np.min(arr), np.max(arr)
@@ -53,17 +32,6 @@ def max_scaler(arr):
     """
     Scales the input array to have the maximum value in the array as the
     scaler.
-
-    Parameters
-    ----------
-    arr : array-like
-        The input array to be scaled.
-
-    Returns
-    -------
-    array-like
-        The scaled array where each element is divided by the maximum value of
-        the input array.
     """
     arr = np.asarray(arr)
     amax = np.max(arr)
@@ -74,17 +42,6 @@ def standar_scaler(arr):
     """
     Standardize an array by subtracting the mean and dividing by the standard
     deviation.
-
-    Parameters
-    ----------
-    arr : array-like
-        The input array to be standardized.
-
-    Returns
-    -------
-    array-like
-        The standardized array where each element is the result of subtracting
-        the mean and dividing by the standard deviation of the input array.
     """
     arr = np.asarray(arr)
     mu, sigma = np.mean(arr), np.std(arr)
