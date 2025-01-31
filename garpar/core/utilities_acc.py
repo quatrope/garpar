c464de929a4dbd7a8ef97ca801e7ecf65ca2138b
JBC <jbcabral@unc.edu.ar>
2025-01-31 16:47:55 -0300

    fix headers

--- garpar/core/utilities_acc.py
+++ garpar/core/utilities_acc.py
@@ -1,6 +1,6 @@
 # This file is part of the
 #   Garpar Project (https://github.com/quatrope/garpar).
-# Copyright (c) 2021, 2022, 2023, 2024, Diego Gimenez, Nadia Luczywo,
+# Copyright (c) 2021-2025 Diego Gimenez, Nadia Luczywo,
 # Juan Cabral and QuatroPe
 # License: MIT
 #   Full Text: https://github.com/quatrope/garpar/blob/master/LICENSE
