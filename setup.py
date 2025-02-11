ad27dc226fc81ffb32740d2239d48eaf2920ba96
juanbc <jbcabral@unc.edu.ar>
2025-02-11 12:33:06 -0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,24 @@
+# This file is part of the
+#   Garpar Project (https://github.com/quatrope/garpar).
+# Copyright (c) 2021-2025 Diego Gimenez, Nadia Luczywo,
+# Juan Cabral and QuatroPe
+# License: MIT
+#   Full Text: https://github.com/quatrope/garpar/blob/master/LICENSE
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""Setup script for Garpar.
+
+empty for backward compatibility
+
+"""
+
+# =============================================================================
+# IMPORTS and RUN
+# =============================================================================
+
+import setuptools
+
+setuptools.setup()
